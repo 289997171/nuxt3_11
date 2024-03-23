68fcae9e59d7238ede719146f4230df169168525
vicking <289997171@qq.com>
2024-03-23 20:41:58 +0800

    useHead & useServerHead

---
+++ pages/test5.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>
+      useHead & useServerHead
+    </h1>
+    <nav>
+      <NuxtLink to="/test5/test1">Test1</NuxtLink>
+    </nav>
+    <NuxtPage/>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/test5/test1.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    test1
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'HEAD Client'
+})
+
+useServerHead({
+  title: 'HEAD Server'
+})
+</script>
+
+<style scoped>
+
+</style>
