cde975905c8bf61ccedfd046b2160ea2392d1d47
vicking <289997171@qq.com>
2024-03-25 17:26:20 +0800

    特殊处理admin

---
+++ pages/admin/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    后台管理页面
+  </div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  middleware: (to, from) => {
+    // 访问admin,必须 /admin?key=xxx 才允许
+    if (to.query.key !== 'xxx') {
+      return navigateTo('/')
+      // showError({
+      //   status: 404,
+      //   message: '找不到页面'
+      // })
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
