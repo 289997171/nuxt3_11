9cd1aa99ee20ff0a2c1325dcdcf7e58d4035111c
vicking <289997171@qq.com>
2024-03-27 08:54:49 +0800

    通用的布局风格

--- app.vue
+++ app.vue
@@ -2,22 +2,7 @@
   <Html>
     <Body>
       <NuxtLoadingIndicator/>
-
-      <div class="flex flex-row">
-        <!--Left-->
-        <aside class="w-64 h-100vh sticky top-0 overflow-y-scroll" >
-          <ul>
-            <li v-for="idx in 100">{{idx}}</li>
-          </ul>
-        </aside>
-
-        <!--Right-->
-        <aside class="flex-1 bg-yellow">
-          <header class="h-20 bg-black sticky top-0"></header>
-          <NuxtPage class="min-h-[calc(100vh-240px)] bg-pink container mx-auto"/>
-          <footer class="h-40 bg-green">123</footer>
-        </aside>
-      </div>
+      <NuxtPage class="container mx-auto"/>
     </Body>
   </Html>
 </template>
---
+++ app2.vue
@@ -0,0 +1,25 @@
+<template>
+  <Html>
+  <Body>
+  <NuxtLoadingIndicator/>
+
+  <div class="flex flex-row">
+    <!--Left-->
+    <aside class="w-64 h-100vh sticky top-0 overflow-y-scroll" >
+      <ul>
+        <li v-for="idx in 100">{{idx}}</li>
+      </ul>
+    </aside>
+
+    <!--Right-->
+    <aside class="flex-1 bg-yellow">
+      <header class="h-20 bg-black sticky top-0"></header>
+      <NuxtPage class="min-h-[calc(100vh-240px)] bg-pink container mx-auto"/>
+      <footer class="h-40 bg-green">123</footer>
+    </aside>
+  </div>
+  </Body>
+  </Html>
+</template>
+<script setup lang="ts">
+</script>
